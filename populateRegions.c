05bf0092c951cdc0b0e4ab551b38b35212150d50
chriskane <cfkane@syr.edu>
2018-03-21 22:40:58 -0400

    Beginning to implement algorithm to restructure how regions are indexed in the region array.

--- populateRegions.c
+++ populateRegions.c
@@ -29,7 +29,33 @@ void recurse_divide_by_mass(region &r){
     }
   }
 }
- 
+
+int findRegionNumber(int index, int index_array[], int &index_array_counter){
+
+  int i;
+  int the_final_index;
+
+  if(index_array_counter == 0){
+    the_final_index = 0;
+  }
+
+  else{//loop through and see if it is new or old
+    for(i=0; i<index_array_counter; i++){
+      if(index == index_array[i]){
+	break;//found an index that matches current one
+      }
+    }
+    if(i==index_array_counter){//this is a new index
+      index_array_counter++;
+      the_final_index = index_array_counter;
+    }
+    else{
+      the_final_index = i;
+    }
+  }
+  return the_final_index;
+}
+
 //===============================
 void populateRegions(planet BD[], int totalIndeces){
   
@@ -39,6 +65,8 @@ void populateRegions(planet BD[], int totalIndeces){
   int z,sum;
   int plnt_num;
   int index;
+  int index_array[1000];
+  int index_array_counter;
   double plnt_mass;
   vector plnt_COM;
   
@@ -47,6 +75,10 @@ void populateRegions(planet BD[], int totalIndeces){
   
   for( i=0; i<LVL; i++ ){
     //printf("!POPREG: working on level %d\n",i);
+    index_array_counter=0;
+    for( j=0; j<1000; j++ ){//neg means no index there
+      index_array[j] = -1;
+    }
     n = pow(2,i);//n is number of boxes per side		
     for( j=0; j<N; j++ ){
       
@@ -58,8 +90,14 @@ void populateRegions(planet BD[], int totalIndeces){
       clamp(c,n);
       z = a + b*n + c*n*n;
       index = getArrayIndex(i,z);
-      BD[j].level[i] = index;//planet is in box index on level i
-            
+      BD[j].level[i] = findRegionNumber(index, index_array, index_array_counter);
+      //so essentially, on level i, I know I am in the BD[j].level[i]th region
+      //in the linked list. Start counting at zero.
+      
+      //BD[j].level[i] = index;//planet is in box 'index' on level 'i'
+      //have a counter that counts which region we are looking at?
+      //loop through this counter?
+      //or match an index with 
       if(regions[index].numPln == 0){	
 	regions[index].mass  = 0;
 	regions[index].com.x = 0;
@@ -73,6 +111,17 @@ void populateRegions(planet BD[], int totalIndeces){
       regions[index].numPln++;
       
     }
+    /*
+      check here to see if the children have planets in them
+      if(yes) make the children of this region
+      if(no) do not make data structure for the children of this region
+     */
+
+    /*
+      or make all children before hand
+      if(yes) keep
+      if(no) delete this from the list
+    */
   }
   recurse_divide_by_mass(regions[0]);
 }
